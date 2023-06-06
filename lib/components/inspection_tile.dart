e4c76d8b7eef880dad6dca47daa9c927647cad9e
Hugo Fabris <hugomfabris@gmail.com>
2023-06-06 17:33:27 -0300

    Rebuild of inspection_tile.dart module
    Created the buildTile function to replace tabletTile, desktopTile and phoneTile to make the code smaller and more organized. Also added the new openFile function in the IconButton widgets to referenciate the url of file attached to each inspection.

--- lib/components/inspection_tile.dart
+++ lib/components/inspection_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:bcsantos/controllers/inspection_controller.dart';
 // import 'package:bcsantos/services/shell_execute_service.dart';
@@ -44,310 +46,139 @@ class InspectionTileState extends State<InspectionTile> {
   Widget build(BuildContext context) {
     String platform = widget.platform;
     if (platform == 'mobile') {
-      return phoneTile();
-    } else if (platform == 'desktop') {
-      return desktopTile();
+      return buildTile(Text(widget.inspection.shipName!), 
+      Center(
+        child: Column(
+      children: [
+        Text(dateFormat.format(widget.inspection.inspectionDate!)),
+        Text(widget.inspection.inspector!),
+      ],
+    )));
     } else {
-      return tabletTile();
+      return buildTile(
+          DataTable(columns: const [
+              DataColumn(label: Text('BC/RB', textAlign: TextAlign.center)),
+              DataColumn(label: Text('INSPETOR', textAlign: TextAlign.center)),
+              DataColumn(label: Text('ANOTAÇÕES', textAlign: TextAlign.center)),
+              DataColumn(label: Text('INSPEÇÃO', textAlign: TextAlign.center)),
+              DataColumn(label: Text('DATA', textAlign: TextAlign.center))
+            ], rows: [
+              DataRow(cells: [
+                DataCell(Text(widget.inspection.shipName!,
+                    textAlign: TextAlign.center)),
+                DataCell(Text(widget.inspection.inspector!,
+                    textAlign: TextAlign.center)),
+                DataCell(Text(widget.inspection.anotations.toString(),
+                    textAlign: TextAlign.center)),
+                DataCell(Text(widget.inspection.inspectionType!,
+                    textAlign: TextAlign.center)),
+                DataCell(Text(
+                  dateFormat.format(widget.inspection.inspectionDate!),
+                )),
+              ])
+            ]),
+            null);
     }
   }
 
-  phoneTile() {
+  void openFile(url) async {
+    final fileReference = FirebaseStorage.instance.refFromURL(url);
+    final downloadURL = await fileReference.getDownloadURL();
+    print(downloadURL);
+
+    final anchorElement = AnchorElement(href: downloadURL)
+      ..setAttribute('target', '_blank')
+      ..setAttribute('rel', 'noopener noreferrer');
+    anchorElement.click();
+  }
+
+  buildTile(Widget data, Widget? subtitle) {
     return Card(
         child: ListTile(
             title: Center(
-              child: Text(widget.inspection.shipName!),
+              child: data,
             ),
             leading: CircleAvatar(child: Text(widget.inspection.inspector![0])),
-            subtitle: Center(
-                child: Column(
-              children: [
-                Text(dateFormat.format(widget.inspection.inspectionDate!)),
-                Text(widget.inspection.inspector!),
-              ],
-            )),
-            // trailing: Wrap(children: [
-            //   IconButton(
-            //       icon: const Icon(Icons.workspace_premium_sharp),
-            //       onPressed: () {
-            //         ShellExecuteService shellExecuteService =
-            //             ShellExecuteService();
-            //         void openFile(path) async {
-            //           await shellExecuteService.openFile(path);
-            //         }
 
-            //         if (widget.inspection.certificate == null) {
-            //           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            //               backgroundColor: Colors.indigo,
-            //               duration: Duration(seconds: 2),
-            //               content: Text('Inspeção não possui certificado')));
-            //         } else {
-            //           return openFile(widget.inspection.certificate);
-            //         }
-            //       }),
-            //   IconButton(
-            //       icon: const Icon((Icons.assignment)),
-            //       onPressed: () {
-            //         ShellExecuteService shellExecuteService =
-            //             ShellExecuteService();
-            //         void openFile(path) async {
-            //           await shellExecuteService.openFile(path);
-            //         }
-
-            //         if (widget.inspection.checklist == null) {
-            //           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            //               backgroundColor: Colors.indigo,
-            //               duration: Duration(seconds: 2),
-            //               content: Text('Inspeção não possui checklist')));
-            //         } else {
-            //           return openFile(widget.inspection.checklist);
-            //         }
-            //       })
-            // ]),
-            onTap: () {
-              showDialog( 
-                  context: context,
-                  barrierDismissible: true,
-                  builder: (context) {
-                    return ListView(children: [
-                      AlertDialog(
-                      title: Center(child: SelectableText(widget.inspection.shipName!)),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [       
-                          SelectableText('Data: ${dateFormat.format(widget.inspection.inspectionDate!)}'),
-                          const SizedBox(height: 10),
-                          SelectableText('Inspetor: ${widget.inspection.inspector!}'),
-                          const SizedBox(height: 10),
-                          SelectableText('Inspeção: ${widget.inspection.inspectionType!}'),
-                          const SizedBox(height: 30),
-                          SelectableText(widget.inspection.observations!)
-                          ],
-                      ),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-                                deleteInspection();
-                              });
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Deletar')),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Fechar'))
-                      ],
-                      actionsAlignment: MainAxisAlignment.center,
-                    )]);
-                  });
-            }));
-  }
-
-  desktopTile() {
-    return Card(
-        child: ListTile(
-            title: Center(
-                child: DataTable(columns: const [
-              DataColumn(label: Text('BC/RB', textAlign: TextAlign.center)),
-              DataColumn(label: Text('INSPETOR', textAlign: TextAlign.center)),
-              DataColumn(label: Text('ANOTAÇÕES', textAlign: TextAlign.center)),
-              DataColumn(label: Text('INSPEÇÃO', textAlign: TextAlign.center)),
-              DataColumn(label: Text('DATA', textAlign: TextAlign.center))
-            ], rows: [
-              DataRow(cells: [
-                DataCell(
-                    Text(widget.inspection.shipName!, textAlign: TextAlign.center)),
-                DataCell(Text(widget.inspection.inspector!,
-                    textAlign: TextAlign.center)),
-                DataCell(Text(widget.inspection.anotations.toString(),
-                    textAlign: TextAlign.center)),
-                DataCell(Text(widget.inspection.inspectionType!,
-                    textAlign: TextAlign.center)),
-                DataCell(Text(
-                  dateFormat.format(widget.inspection.inspectionDate!),
-                )),
-              ])
-            ])),
-            leading: CircleAvatar(
-              child: Text(widget.inspection.inspector![0]),
-            ),
-            dense: true,
-            // trailing: Wrap(children: [
-            //   IconButton(
-            //       icon: const Icon(Icons.workspace_premium_sharp),
-            //       onPressed: () {
-            //         ShellExecuteService shellExecuteService =
-            //             ShellExecuteService();
-            //         void openFile(path) async {
-            //           await shellExecuteService.openFile(path);
-            //         }
-
-            //         if (widget.inspection.certificate == null) {
-            //           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            //               backgroundColor: Colors.indigo,
-            //               duration: Duration(seconds: 2),
-            //               content: Text('Inspeção não possui certificado')));
-            //         } else {
-            //           return openFile(widget.inspection.certificate);
-            //         }
-            //       }),
-            //   IconButton(
-            //       icon: const Icon((Icons.assignment)),
-            //       onPressed: () {
-            //         ShellExecuteService shellExecuteService =
-            //             ShellExecuteService();
-            //         void openFile(path) async {
-            //           await shellExecuteService.openFile(path);
-            //         }
-
-            //         if (widget.inspection.checklist == null) {
-            //           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            //               backgroundColor: Colors.indigo,
-            //               duration: Duration(seconds: 2),
-            //               content: Text('Inspeção não possui checklist')));
-            //         } else {
-            //           return openFile(widget.inspection.checklist);
-            //         }
-            //       })
-            // ]),
+            subtitle: subtitle,
+            trailing: Wrap(children: [
+              IconButton(
+                  icon: const Icon(Icons.workspace_premium_sharp),
+                  onPressed: () {
+                    if (widget.inspection.certificate == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            backgroundColor: Colors.indigo,
+                            duration: Duration(seconds: 2),
+                            content: Text('Inspeção não possui certificado')),
+                      );
+                    } else {
+                      openFile(widget.inspection.certificate);
+                    }
+                  }),
+              IconButton(
+                  icon: const Icon(Icons.assignment),
+                  onPressed: () {
+                    if (widget.inspection.checklist == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            backgroundColor: Colors.indigo,
+                            duration: Duration(seconds: 2),
+                            content: Text('Inspeção não possui checklist')),
+                      );
+                    } else {
+                      openFile(widget.inspection.checklist);
+                    }
+                  }),
+            ]),
             onTap: () {
               showDialog(
                   context: context,
                   barrierDismissible: true,
                   builder: (context) {
-                    return AlertDialog(
-                      title: Center(child: Text(widget.inspection.shipName!)),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SelectableText('Data: ${dateFormat.format(widget.inspection.inspectionDate!)}'),
-                          const SizedBox(height: 10),
-                          SelectableText('Inspetor: ${widget.inspection.inspector!}'),
-                          const SizedBox(height: 10),
-                          SelectableText('Inspeção: ${widget.inspection.inspectionType!}'),
-                          const SizedBox(height: 30),
-                          SelectableText(widget.inspection.observations!)
-                          ],
-                      ),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-                                deleteInspection();
-                              });
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Deletar')),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Fechar'))
-                      ],
-                      actionsAlignment: MainAxisAlignment.center,
-                    );
-                  });
-            }));
-  }
-
-  tabletTile() {
-    return Card(
-        child: ListTile(
-            title: Center(
-                child: DataTable(columns: const [
-              DataColumn(label: Text('BC/RB', textAlign: TextAlign.center)),
-              DataColumn(label: Text('INSPETOR', textAlign: TextAlign.center)),
-              DataColumn(label: Text('ANOTAÇÕES', textAlign: TextAlign.center)),
-              DataColumn(label: Text('INSPEÇÃO', textAlign: TextAlign.center)),
-              DataColumn(label: Text('DATA', textAlign: TextAlign.center))
-            ], rows: [
-              DataRow(cells: [
-                DataCell(
-                    Text(widget.inspection.shipName!, textAlign: TextAlign.center)),
-                DataCell(Text(widget.inspection.inspector!,
-                    textAlign: TextAlign.center)),
-                DataCell(Text(widget.inspection.anotations.toString(),
-                    textAlign: TextAlign.center)),
-                DataCell(Text(widget.inspection.inspectionType!,
-                    textAlign: TextAlign.center)),
-                DataCell(Text(
-                  dateFormat.format(widget.inspection.inspectionDate!),
-                )),
-              ])
-            ])),
-            leading: CircleAvatar(
-              child: Text(widget.inspection.inspector![0]),
-            ),
-            dense: true,
-            // trailing: Wrap(children: [
-            //   IconButton(
-            //       icon: const Icon(Icons.workspace_premium_sharp),
-            //       onPressed: () {
-            //         ShellExecuteService shellExecuteService =
-            //             ShellExecuteService();
-            //         void openFile(path) async {
-            //           await shellExecuteService.openFile(path);
-            //         }
-
-            //         if (widget.inspection.certificate == null) {
-            //           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            //               backgroundColor: Colors.indigo,
-            //               duration: Duration(seconds: 2),
-            //               content: Text('Inspeção não possui certificado')));
-            //         } else {
-            //           return openFile(widget.inspection.certificate);
-            //         }
-            //       }),
-            //   IconButton(
-            //       icon: const Icon((Icons.assignment)),
-            //       onPressed: () {
-            //         ShellExecuteService shellExecuteService =
-            //             ShellExecuteService();
-            //         void openFile(path) async {
-            //           await shellExecuteService.openFile(path);
-            //         }
-
-            //         if (widget.inspection.checklist == null) {
-            //           ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            //               backgroundColor: Colors.indigo,
-            //               duration: Duration(seconds: 2),
-            //               content: Text('Inspeção não possui checklist')));
-            //         } else {
-            //           return openFile(widget.inspection.checklist);
-            //         }
-            //       })
-            // ]),
-            onTap: () {
-              showDialog(
-                  context: context,
-                  barrierDismissible: true,
-                  builder: (context) {
-                    return AlertDialog(
-                      title: Center(child: Text(widget.inspection.shipName!)),
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [Text(widget.inspection.observations!)],
-                      ),
-                      actions: [
-                        TextButton(
-                            onPressed: () {
-                              setState(() {
-                                deleteInspection();
-                              });
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Deletar')),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Fechar'))
-                      ],
-                      actionsAlignment: MainAxisAlignment.center,
-                    );
-                  });
-            }));
-  }
+                    return GestureDetector (
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child:
+                        ListView(children: [
+                          AlertDialog(
+                            title: Center(
+                                child: SelectableText(widget.inspection.shipName!)),
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                SelectableText(
+                                    'Data: ${dateFormat.format(widget.inspection.inspectionDate!)}'),
+                                const SizedBox(height: 10),
+                                SelectableText(
+                                    'Inspetor: ${widget.inspection.inspector!}'),
+                                const SizedBox(height: 10),
+                                SelectableText(
+                                    'Inspeção: ${widget.inspection.inspectionType!}'),
+                                const SizedBox(height: 30),
+                                SelectableText(widget.inspection.observations!)
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      deleteInspection();
+                                    });
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Deletar')),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text('Fechar'))
+                            ],
+                            actionsAlignment: MainAxisAlignment.center,
+                          )
+                        ]));
+                      });
+                }));
+              }
 }
