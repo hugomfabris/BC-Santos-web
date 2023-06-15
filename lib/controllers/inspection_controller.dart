f24fde47dea3fc57c7097c9223d783fed762c140
Hugo Fabris <hugomfabris@gmail.com>
2023-06-15 15:41:36 -0300

    Update deleteInspection function inside inspection_controller.dart
    Added logic to make the function exclude the file added to the firebase storage when the inspection is deleted

--- lib/controllers/inspection_controller.dart
+++ lib/controllers/inspection_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:bcsantos/models/inspection.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class InspectionController extends ChangeNotifier {
@@ -23,12 +24,13 @@ class InspectionController extends ChangeNotifier {
         .collection('inspections')
         .snapshots()
         .listen((QuerySnapshot<Map<String, dynamic>> data) {
-          _inspections.clear();
+      _inspections.clear();
       for (final document in data.docs) {
         final inspection = Inspection.fromDocument(document);
         inspections.add(inspection);
       }
-      _inspections.sort((a, b) => b.inspectionDate!.compareTo(a.inspectionDate!));
+      _inspections
+          .sort((a, b) => b.inspectionDate!.compareTo(a.inspectionDate!));
       notifyListeners();
     });
   }
@@ -43,9 +45,7 @@ class InspectionController extends ChangeNotifier {
   addInspection(Inspection inspection) {
     inspections.add(inspection);
     notifyListeners();
-    FirebaseFirestore.instance
-        .collection('inspections')
-        .add(<String, dynamic>{
+    FirebaseFirestore.instance.collection('inspections').add(<String, dynamic>{
       'shipName': inspection.shipName,
       'inspector': inspection.inspector,
       'inspectionType': inspection.inspectionType,
@@ -57,12 +57,44 @@ class InspectionController extends ChangeNotifier {
     });
   }
 
-  deleteInspection(Inspection inspection) {
-    inspections.remove(inspection);
-    notifyListeners();
-    FirebaseFirestore.instance
-        .collection('inspections')
-        .doc(inspection.id)
-        .delete();
+  deleteInspection(Inspection inspection) async {
+    try {
+      final storageRef = FirebaseStorage.instance.ref();
+      if (inspection.certificate != null) {
+        final fileToDelete = storageRef.child(
+            'certificados/${inspection.shipName!.toLowerCase()}/${inspection.inspectionDate.toString()} - certificado ${inspection.shipName!.toLowerCase()}.pdf}');
+        try {
+          await fileToDelete.delete();
+          print('Certificado deletado');
+        } catch (e) {
+          print(e);
+        }
+      }
+      else {
+        print('Certificado não encontrado');
+      }
+      if (inspection.checklist != null) {
+        final fileToDelete = storageRef.child(
+            'checklists/${inspection.shipName!.toLowerCase()}/${inspection.inspectionDate.toString()} - checklist ${inspection.shipName!.toLowerCase()}.pdf}');
+        try {
+          await fileToDelete.delete();
+        } catch (e) {
+          print(e);
+        }
+      }
+      // Delete the document inside the Firebase Firestore
+      await FirebaseFirestore.instance
+          .collection('inspections')
+          .doc(inspection.id)
+          .delete();
+
+      // Remove the inspection from the local list and notify listeners
+      inspections.remove(inspection);
+      notifyListeners();
+
+      print('Inspection deleted');
+    } catch (e) {
+      print(e);
+    }
   }
 }
