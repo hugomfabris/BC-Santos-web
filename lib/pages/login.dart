6c6b75f3f3f7cde29872c8c64b2ae2d6c5853c31
Hugo Fabris <hugomfabris@gmail.com>
2023-06-20 12:05:59 -0300

    Added Provider package to login.dart body inside build method
    This important change has the propose to read the login state of the application in a more secure, stable and efficient manner

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,6 +1,9 @@
 import 'package:bcsantos/pages/home_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import '../app_state.dart';
 
 class Login extends StatefulWidget {
   const Login({
@@ -13,7 +16,6 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final GlobalKey<FormState> _formKey = GlobalKey();
-
   final FocusNode _focusNodePassword = FocusNode();
   final TextEditingController _controllerUsername = TextEditingController();
   final TextEditingController _controllerPassword = TextEditingController();
@@ -21,97 +23,118 @@ class _LoginState extends State<Login> {
   bool _obscurePassword = true;
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
-      if (user != null) {
-        return const MyHomePage(title: 'BC SANTOS');
-      }
-
+    if (user != null) {
+      return const MyHomePage(title: 'BC SANTOS');
+    }
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
-              Text(
-                "BC SANTOS",
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              const SizedBox(height: 10),
-              Text(
-                "Entre na sua conta",
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              const SizedBox(height: 60),
-              TextFormField(
-                controller: _controllerUsername,
-                keyboardType: TextInputType.name,
-                decoration: InputDecoration(
-                  labelText: "Usuário",
-                  prefixIcon: const Icon(Icons.person_outline),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+      body: Provider<ApplicationState>(
+        create: (_) => ApplicationState(), // Ou forneça uma instância existente se necessário
+        child:Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 50),
+                Text(
+                  "BC SANTOS",
+                  style: Theme.of(context).textTheme.headlineLarge,
                 ),
-                onEditingComplete: () => _focusNodePassword.requestFocus(),
-  
-              ),
-              const SizedBox(height: 10),
-              TextFormField(
-                controller: _controllerPassword,
-                focusNode: _focusNodePassword,
-                obscureText: _obscurePassword,
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  labelText: "Senha",
-                  prefixIcon: const Icon(Icons.password_outlined),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _obscurePassword = !_obscurePassword;
-                        });
-                      },
-                      icon: _obscurePassword
-                          ? const Icon(Icons.visibility_outlined)
-                          : const Icon(Icons.visibility_off_outlined)),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                const SizedBox(height: 10),
+                Text(
+                  "Entre na sua conta",
+                  style: Theme.of(context).textTheme.bodyMedium,
                 ),
-              ),
-              const SizedBox(height: 60),
-              Column(
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      minimumSize: const Size.fromHeight(50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+                const SizedBox(height: 60),
+                TextFormField(
+                  controller: _controllerUsername,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    labelText: "Usuário",
+                    prefixIcon: const Icon(Icons.person_outline),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    onPressed: () async {
-                      if (_formKey.currentState?.validate() ?? false) {
-                        try {
-                          final userCredential =
-                              await _auth.signInWithEmailAndPassword(
-                            email: _controllerUsername.text,
-                            password: _controllerPassword.text,
-                          );
-
-                          if (userCredential.user != null) {
-                            await userCredential.user?.reload();
-
-                            Navigator.pushReplacement(
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onEditingComplete: () => _focusNodePassword.requestFocus(),
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _controllerPassword,
+                  focusNode: _focusNodePassword,
+                  obscureText: _obscurePassword,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    labelText: "Senha",
+                    prefixIcon: const Icon(Icons.password_outlined),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _obscurePassword = !_obscurePassword;
+                          });
+                        },
+                        icon: _obscurePassword
+                            ? const Icon(Icons.visibility_outlined)
+                            : const Icon(Icons.visibility_off_outlined)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 60),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          try {
+                            final userCredential =
+                                await _auth.signInWithEmailAndPassword(
+                              email: _controllerUsername.text,
+                              password: _controllerPassword.text,
+                            );
+                            if (userCredential.user != null) {
+                              GoRouter.of(context).go('/home');
+                              print('Usuário logado com sucesso!');
+                            }
+                          } catch (e) {
+                            // Trate os erros de autenticação aqui, por exemplo, exiba uma mensagem de erro.
+                            print('Erro ao fazer login: $e');
+                          }
+                        }
+                      },
+                      child: const Text("Login"),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Deseja retornar à home page?"),
+                        TextButton(
+                          onPressed: () {
+                            _formKey.currentState?.reset();
+                            GoRouter.of(context).go('/');
+                            Navigator.push(
                               context,
                               MaterialPageRoute(
                                 builder: (context) {
@@ -119,43 +142,18 @@ class _LoginState extends State<Login> {
                                 },
                               ),
                             );
-                          }
-                        } catch (e) {
-                          // Trate os erros de autenticação aqui, por exemplo, exiba uma mensagem de erro.
-                          print('Erro ao fazer login: $e');
-                        }
-                      }
-                    },
-                    child: const Text("Login"),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text("Deseja retornar à home page?"),
-                      TextButton(
-                        onPressed: () {
-                          _formKey.currentState?.reset();
-
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const MyHomePage(title: 'BC SANTOS');
-                              },
-                            ),
-                          );
-                        },
-                        child: const Text("Retornar"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                          },
+                          child: const Text("Retornar"),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+    ));
   }
 
   @override
